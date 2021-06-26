9609a4f4f5287bb4cfc88000b7fdf6166304ba3f
tosh <tosh.the@yandex.ru>
2021-06-26 14:33:36 +0300

    init fixtures

--- edu_stock/stock/models.py
+++ edu_stock/stock/models.py
@@ -3,10 +3,10 @@ from django.core.validators import RegexValidator
 
 
 class Currency(models.Model):
-    name = models.CharField(unique=True, max_length=255)
-    identifier = models.CharField(max_length=4, validators=[RegexValidator(regex=r'^\w{2,4}$', message='Identifier length has to be 2-4 characters in length')], primary_key=True)
+    name = models.CharField(unique = True, max_length = 255)
+    identifier = models.CharField(max_length = 4, validators = [RegexValidator(regex = r'^\w{2,4}$', message = 'Identifier length has to be 2-4 characters in length')], primary_key = True)
 
 
 class Order(models.Model):
-    account = models.ForeignKey('accounts.Account', related_name="orders", on_delete=models.CASCADE)
-    amount = models.DecimalField(decimal_places=2, max_digits=20)
+    account = models.ForeignKey('accounts.Account', related_name = "orders", on_delete = models.CASCADE)
+    amount = models.DecimalField(decimal_places = 2, max_digits = 20)
