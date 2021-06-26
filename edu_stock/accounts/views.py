20f11eb6d99b14096c05250858df5b192af849a4
Vladimir Shemankov <vladimir.shemankov@gmail.com>
2021-06-26 14:54:30 +0300

    add generic views

--- edu_stock/accounts/views.py
+++ edu_stock/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
 
-# Create your views here.
+from .models import Account
+from .serializers import AccountSerializer
+
+
+class AccountView(generics.ListAPIView):
+    """
+    API endpoint that allows listing accounts
+    """
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = AccountSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Account.objects.filter(user=user)
--- edu_stock/stock/views.py
+++ edu_stock/stock/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
 
-# Create your views here.
+from .models import Currency, Order
+from .serializers import CurrencySerializer, OrderSerializer
+
+
+class CurrencyView(generics.ListAPIView):
+    """
+    API endpoint that allows listing currencies, in alphabetical order
+    """
+    queryset = Currency.objects.all().order_by('id')
+    serializer_class = CurrencySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class OrderView(generics.ListCreateAPIView):
+    """
+    API endpoint that allows listing and creating orders
+    """
+    queryset = Order.objects.all().order_by('-id')
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
